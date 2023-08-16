3076ff76d067406b10f5b253f7b40754d43aca23
shiva kumar <98257507+Shivuduu19@users.noreply.github.com.com>
2023-08-16 22:16:45 +0530

    added remove book btn functionality

--- library.js
+++ library.js
@@ -1,5 +1,6 @@
 /* eslint-disable indent */
 const body = document.querySelector("body");
+//const init= new Book("shivuduu","shiva",55,"true")
 const myLibrary = [];
 function Book(title, author, pages, read) {
   this.title = title;
@@ -12,10 +13,10 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 function displayBooks() {
-    const bookAllDiv = document.getElementById(book-list);
+    const bookAllDiv = document.getElementById("book-list");
    bookAllDiv.innerHTML='';
 
-   myLibrary.forEach((book)=>{
+   myLibrary.forEach((book,index)=>{
    const bookDiv=document.createElement('div');
    bookDiv.classList.add('book-card');
 
@@ -23,7 +24,40 @@ function displayBooks() {
    <h2>${book.title}</h2>
    <p>Author:${book.author}</p>
    <p>pages:${book.pages}</p>
-   <p>status:${book.read}?'Read':'Not Read</p>`;
+   <p>status:${book.read ? 'Read':'Not Read'}</p>
+   <button class='remove-book-btn' data-index='${index}'>remove book</button>
+   `
+   
    bookAllDiv.appendChild(bookDiv);
    });
 }
+
+document.getElementById("new-book-btn").addEventListener('click',
+()=>{document.getElementById("new-book-form").style.display='block'})
+
+document.getElementById('add-book-btn').addEventListener('click',()=>
+{
+  const title  = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages  = document.getElementById('pages').value;
+  const read   = document.getElementById('read').checked;
+  const newBook = new Book(title,author,pages,read);
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+  displayBooks();
+  document.getElementById('new-book-form').style.display='none';
+  document.getElementById('title').value='';
+  document.getElementById('author').value='';
+  document.getElementById('pages').value='';
+  document.getElementById('read').checked=false;
+
+})
+document.addEventListener('click',(e)=>
+{
+  if(e.target.classList.contains('remove-book-btn')){
+  const index= e.target.getAttribute('data-index')
+  myLibrary.splice(index,1);
+  displayBooks();
+  }
+}
+)
